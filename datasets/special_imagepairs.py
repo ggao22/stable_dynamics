6a50f873ab04331ef5da2bf04b7611cd2fabe50f
ggao22 <g.gao@wustl.edu>
2024-05-22 16:17:03 -0400

    modifing for robo dataset

---
+++ datasets/special_imagepairs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from . import images
+import numpy as np
+import torch
+from torchvision import transforms
+
+class SpecialSeqPairs(torch.utils.data.Dataset):
+    def __init__(self, npz_path, transform=transforms.Compose([transforms.ToTensor(), transforms.Resize(256,256)])):
+        super().__init__()
+        data = np.load(npz_path)
+        self.image_dataset = transform(np.moveaxis(data['images'], 3, 1))
+        self.end_indices = data['motion_start'].astype(int) - 1
+        self.end_indices[0] = len(self.image_dataset) - 1
+
+    def __getitem__(self, index):
+        if index not in self.end_indices:
+            q1 = self.image_dataset[index]
+            q2 = self.image_dataset[index + 1]
+        else:
+            q1 = q2 = self.image_dataset[index]
+        return ((q1, q2), (q1, q2))
+
+    def __len__(self):
+        return len(self.image_dataset) - 1
+
+def build(props):
+    return SpecialSeqPairs(props['file'])
