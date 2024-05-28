242189869baed5ef88480d25c4d9f208e298decc
ggao22 <g.gao@wustl.edu>
2024-05-28 15:51:20 -0400

    num img correction

--- models/vae.py
+++ models/vae.py
@@ -14,7 +14,7 @@ class VAE(nn.Module):
     def __init__(self, LATENT_SPACE_DIM=320, num_imgs=1):
         super().__init__()
 
-        self.size_after_conv = [256, 16*num_imgs, 16*num_imgs]
+        self.size_after_conv = [256, 16*num_imgs, 16]
         self.inter_size = 512
 
         self.fc_e1 = nn.Conv2d(3, 32, kernel_size=4, padding=1, stride=2)
